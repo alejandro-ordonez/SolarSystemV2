a99cc78037a8bbb25e96c7e5811265d0b48e754b
CERI <jhona.urbanod@utadeo.edu.co>
2019-06-10 11:55:15 -0500

    Ip Added

--- ESP32/Prueba/src/Solar.h
+++ ESP32/Prueba/src/Solar.h
@@ -12,26 +12,11 @@ class Solar{
    }
    void setTemp(float t){
        Temp=t;
-   } 
-  return(Voltage);
-   }
-   float getVoltageinPos(int i){
-       return(Voltage[i]);
-   }
-   float getCurrentinPos(int i){
-       return(Current[i]);
-   }
-   int getSizeCurrent(){
-       return(Current.Capacity());
    }
    float getIR(){
        return(Radiaton);
    }
    float getT(){
        return(Temp);
-   }
-   void addCurrentAndVoltage(float I, float V){
-       Current.Add(I);
-       Voltage.Add(V);
    }
 };
