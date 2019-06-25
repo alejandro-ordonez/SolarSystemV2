af9e4f5ec10151a514296d48da3390557e98fae6
CERI <jhona.urbanod@utadeo.edu.co>
2019-06-25 11:19:10 -0500

    Voltage_added

--- ESP32/Prueba_Sensor_Voltage/src/main.cpp
+++ ESP32/Prueba_Sensor_Voltage/src/main.cpp
@@ -1,14 +1,24 @@
 #include <Arduino.h>
+
 double R1 =1200000.0;
 double R2 =1000000.0;
 int VF =11.3;
 int VT =0.265;
-const double VTrans=25;
-const double VFuent=26;
+int VTrans=25;
+int VFuent=26;
 double Req=0 ;
 double VoltajeDif=0 ;
 double Voltaje=0 ;
 void setup() {
+  adcAttachPin(VTrans);
+  adcStart(VTrans);
+  analogReadResolution(11);       
+  analogSetAttenuation(ADC_11db);   
+
+  adcAttachPin(VFuent);
+  adcStart(VFuent);
+  analogReadResolution(11);       
+  analogSetAttenuation(ADC_11db);  
 
  Serial.begin (9600);
 
@@ -17,22 +27,42 @@ void setup() {
 }
 
 void loop() {
-
-double ValueFuent = analogRead(VFuent)* (VF/ 4096.0); 
-double ValueTrans = analogRead(VTrans)* (VT/ 4096.0); 
-float VoltajeSensorFuent =(ValueFuent/Req);
-float VoltajeSensorTrans = (ValueTrans/Req);
+    
+double VoltajeSensorFuent =averageAnalogReading(200, VFuent)*(3.3/ 2048.0);
+double VoltajeSensorTrans =averageAnalogReading(200, VTrans)*(3.3/ 2048.0);
 VoltajeDif =VoltajeSensorFuent-VoltajeSensorTrans;
-Voltaje=VoltajeDif;
-delay(1000);
-Serial.print("Voltaje: ");
-Serial.println(Voltaje);
+Voltaje=VoltajeDif*3.6363;
 Serial.print("V1: ");
-Serial.println(analogRead(VFuent));
 Serial.println(VoltajeSensorFuent);
 Serial.print("V2: ");
-Serial.println(analogRead(VTrans));
 Serial.println(VoltajeSensorTrans);
+Serial.print("Voltaje: ");
+Serial.println(Voltaje);
+//double ValueFuent = analogRead(VFuent)* (3.3/ 4096.0); 
+//double ValueTrans = analogRead(VTrans)* (3.3/ 4096.0); 
+//float VoltajeSensorFuent =(ValueFuent/Req);
+//float VoltajeSensorTrans = (ValueTrans/Req);
+//VoltajeDif =VoltajeSensorFuent-VoltajeSensorTrans;
+//Voltaje=VoltajeDif*3.6363;
+//delay(1000);
+//Serial.print("Voltaje: ");
+//Serial.println(Voltaje);
+//Serial.print("V1: ");
+//Serial.println(analogRead(VFuent));
+//Serial.println(VoltajeSensorFuent);
+//Serial.print("V2: ");
+//Serial.println(analogRead(VTrans));
+//Serial.println(VoltajeSensorTrans);
 
-
+}double averageAnalogReading(double samples, int analogPin){
+  float temp = 0;
+  for (size_t i = 0; i < samples; i++)
+  {
+    /* code */
+    temp+=analogRead(analogPin);
+  }
+  return temp/samples;  
 }
+
+
+
