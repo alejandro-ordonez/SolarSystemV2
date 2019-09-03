620bdfc8995f91c4ad1ce8eb0289144bb5b1b5ec
CERI <jhona.urbanod@utadeo.edu.co>
2019-09-03 14:08:22 -0500

    Act

--- ESP32/FilterTest/src/main.ino
+++ ESP32/FilterTest/src/main.ino
@@ -10,34 +10,46 @@
 //float a[]={1.000000000000000, -0.854358985825324};
 
 //15Hz
-float b[]={0.190689333359277, 0.190689333359277};
-float a[]={1.000000000000000, -0.618621333281447};
+//float b[]={0.940148300306698  , 0.940148300306698};
+//float a[]={1.000000000000000, 0.880296600613396};
 
+//10Hz
+float b[]={0.0304590279514212,0.0304590279514212};
+float a[]={1.000000000000000,-0.939081944097158};
 float lp_in[2];
 float lp_out[2];
 float temp = 0;
-long ta=0;
-long tb=0;
+long int ta=0;
+long int tb=0;
 dig_comp filter(b,a, lp_in, lp_out, 2,2);
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
   adcAttachPin(39);
   adcStart(39);
   ledcSetup(0, 50000, 16);
   ledcAttachPin(5, 0);
-  ledcWrite(0,65536);
+  //ledcWrite(0,65536);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  temp = analogRead(39);
-  Serial.print(ReadVoltage(filter.calc_out(temp)));
-  Serial.print(" ");
-  Serial.println(temp);
-  delay(5);
-}
+  for (size_t i = 0; i < 65536; i+=1)
+    { 
+      ta=micros();
+      ledcWrite(0,i);
+      // put your main code here, to run repeatedly:
+      temp = analogRead(39);
+    //Serial.print(ReadVoltage(filter.calc_out(temp)));
+    Serial.print(filter.calc_out(temp),5);
+    Serial.print("    ");
+    Serial.println(temp,5);
+     tb=micros();
+     if((tb-ta)<100){
+       delayMicroseconds(100-(tb-ta));
+     }
+  }
+  }
 
 double ReadVoltage(double reading)
 {
